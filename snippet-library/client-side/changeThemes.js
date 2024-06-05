e9dc7c78673ad724c552bc440bb01446ed6ac51a
FoUnDeR <88034097+FoUnDeRR@users.noreply.github.com>
2024-06-05 16:13:22 +0530

    Added our first snippet :)

---
+++ snippet-library/client-side/changeThemes.js
@@ -0,0 +1,13 @@
+const { ThemeTypes } = findByProps("ThemeTypes")
+findByProps("saveClientTheme").saveClientTheme({ theme: ThemeTypes.THEME_NAME_HERE })
+
+/*
+Available Theme Types {
+                    ThemeTypes.DARK
+                    ThemeTypes.LIGHT
+                    ThemeTypes.MOONLIGHT
+                    ThemeTypes.DARKER
+                }
+
+Snippet Author(s): FoUnDeR, Happyenderman
+*/
